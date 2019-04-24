f6c015ae4685df3ea5679e6f9b4bc6fa0faa9b07
SoFa <felix0eckert0.online.de@gmail.com>
2019-04-24 20:53:55 +0200

    Created "TardfileTemplate" for GSON

---
+++ src/main/java/com/linesix/akhaten/util/TardfileTemplate.java
@@ -0,0 +1,84 @@
+package com.linesix.akhaten.util;
+
+public class TardfileTemplate {
+
+    private String user;
+    private String uuid;
+    private String tardis_id;
+    private String x;
+    private String y;
+    private String z;
+
+    public String getUser() {
+
+        return user;
+
+    }
+
+    public void setUser(String user) {
+
+        this.user = user;
+
+    }
+
+    public String getUuid() {
+
+        return uuid;
+
+    }
+
+    public void setUuid(String uuid) {
+
+        this.uuid = uuid;
+
+    }
+
+    public String getTardis_id() {
+
+        return tardis_id;
+
+    }
+
+    public void setTardis_id(String tardis_id) {
+
+        this.tardis_id = tardis_id;
+
+    }
+
+    public String getX() {
+
+        return x;
+
+    }
+
+    public void setX(String x) {
+
+        this.x = x;
+
+    }
+
+    public String getY() {
+
+        return y;
+
+    }
+
+    public void setY(String y) {
+
+        this.y = y;
+
+    }
+
+    public String getZ() {
+
+        return z;
+
+    }
+
+    public void setZ(String z) {
+
+        this.z = z;
+
+    }
+
+}
