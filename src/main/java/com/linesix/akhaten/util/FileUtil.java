4a3d09956e0a222fef4f6c7a806b038af27caa0f
SoFa <felix0eckert0.online.de@gmail.com>
2019-04-25 12:59:22 +0200

    Added Documentation and type of parameters of Combine to file

--- src/main/java/com/linesix/akhaten/util/FileUtil.java
+++ src/main/java/com/linesix/akhaten/util/FileUtil.java
@@ -13,14 +13,26 @@ import java.util.List;
 
 public class FileUtil {
 
-    public static String combine(String path1, String path2) {
+    /**
+     * Combines 2 File (paths) to one path
+     *
+     * @param path1 first part of the path
+     * @param path2 second part of the path to be combined
+     *
+     */
+    public static String combine(File path1, File path2) {
 
-        File file1 = new File(path1);
-        File file2 = new File(file1, path2);
+        File file2 = new File(path1.getPath(), path2.getPath());
         return file2.getPath();
 
     }
 
+    /**
+     * Parses a json file and returns it as an JsonObject
+     *
+     * @param path Path to the JsonFile
+     *
+     */
     public static JsonObject parseJSON(File path) throws IOException {
 
         String rawData = new String(Files.readAllBytes(Paths.get(path.getPath())));
