10073f62a768eb069ed93fdc76293a1c2a0a3306
SoFa <felix0eckert0.online.de@gmail.com>
2019-04-25 19:59:51 +0200

    Moved to own WorldProvider and renamed VoidRenderer

--- src/main/java/com/linesix/akhaten/dimensions/renderes/TardisDimRenderer.java
+++ src/main/java/com/linesix/akhaten/dimensions/renderes/TardisDimRenderer.java
@@ -4,10 +4,12 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraftforge.client.IRenderHandler;
 
-public class VoidRenderer extends IRenderHandler {
+public class TardisDimRenderer extends IRenderHandler {
+
 
     @Override
     public void render(float partialTicks, WorldClient world, Minecraft mc) {
+
     }
 
 }
--- src/main/java/com/linesix/akhaten/dimensions/worldproviders/WorldProviderTardis.java
+++ src/main/java/com/linesix/akhaten/dimensions/worldproviders/WorldProviderTardis.java
@@ -1,19 +1,12 @@
 package com.linesix.akhaten.dimensions.worldproviders;
 
 import com.linesix.akhaten.dimensions.DimensionTardis;
-import com.linesix.akhaten.dimensions.Dimensions;
 import com.linesix.akhaten.dimensions.chunkgenerators.ChunkGeneratorTardis;
-import com.linesix.akhaten.dimensions.renderes.VoidRenderer;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Biomes;
-import net.minecraft.util.math.BlockPos;
+import com.linesix.akhaten.dimensions.renderes.TardisDimRenderer;
 import net.minecraft.world.DimensionType;
 import net.minecraft.world.WorldProvider;
-import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraftforge.client.IRenderHandler;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class WorldProviderTardis extends WorldProvider {
 
@@ -28,66 +21,6 @@ public class WorldProviderTardis extends WorldProvider {
     }
 
     @Override
-    public float calculateCelestialAngle(long worldTime, float partialTicks) {
-
-        return 0.5F;
-
-    }
-
-    @Override
-    public float[] calcSunriseSunsetColors(float celestialAngle, float partialTicks) {
-
-        return super.calcSunriseSunsetColors(celestialAngle, partialTicks);
-
-    }
-
-    @Override
-    public int getDimension() {
-
-        return super.getDimension();
-
-    }
-
-    @Override
-    public Biome getBiomeForCoords(BlockPos pos) {
-
-        return Biomes.DESERT;
-
-    }
-
-    @Override
-    public void calculateInitialWeather() {}
-
-    @Override
-    protected void generateLightBrightnessTable() {
-
-        for (int i = 0; i <= 15; ++i) {
-
-            float f1 = 1.0F - i / 15.0F;
-            this.lightBrightnessTable[i] = (1.0F - f1) / (f1 * 3.0F + 1.0F) * 1.0F + 0.0F;
-
-        }
-
-    }
-
-    @Override
-    public float getSunBrightness(float par1) {
-
-        return 0.3f;
-
-    }
-
-    @Override
-    public float getSunBrightnessFactor(float par1) {
-
-        return 0;
-
-    }
-
-    @Override
-    public void updateWeather() {}
-
-    @Override
     public DimensionType getDimensionType() {
 
         return DimensionTardis.TARDIS_DIM;
@@ -97,32 +30,15 @@ public class WorldProviderTardis extends WorldProvider {
     @Override
     public boolean canRespawnHere() {
 
-        return false;
+        return true;
 
     }
 
-    @Override
-    public WorldSleepResult canSleepAt(EntityPlayer player, BlockPos pos) {
-
-        return WorldSleepResult.ALLOW;
-
-    }
-
-    @SideOnly(Side.CLIENT)
     @Override
     public IRenderHandler getCloudRenderer() {
 
-        return new VoidRenderer();
+        return new TardisDimRenderer();
 
     }
-
-    @SideOnly(Side.CLIENT)
-    @Override
-    public IRenderHandler getSkyRenderer() {
-
-        return new VoidRenderer();
-
-    }
-
 
 }
