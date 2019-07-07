9f83c806730b6e56ad763376b82bebdf62ac38fe
Bertrahm <felix_eckert_@t-online.de>
2019-07-07 17:08:50 +0200

    Create SilicateIngot.java
    Forgot to commit before

---
+++ src/main/java/com/linesix/akhaten/common/items/materials/SilicateIngot.java
@@ -0,0 +1,13 @@
+package com.linesix.akhaten.common.items.materials;
+
+import com.linesix.akhaten.common.items.ItemBase;
+import com.linesix.akhaten.common.items.ItemNames;
+import com.linesix.akhaten.common.items.registries.Materials;
+
+public class SilicateIngot extends ItemBase {
+
+	public SilicateIngot() {
+		super(ItemNames.Materials.silicate_ingot, ItemNames.Materials.silicate_ingot, Materials.materialtab);
+	}
+
+}
